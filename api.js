a6e7781fe296873f0dc1ac521db109537d635c0e
alicia-so <aliciaolvra@gmail.com>
2022-07-10 16:45:27 -0300

    feat: alterações js e styles

--- api.js
+++ api.js
@@ -4,3 +4,4 @@ export const API_KEY = '05348e9ad10d8405bd4e51119e3946f2';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
+
--- main.js
+++ main.js
@@ -3,3 +3,42 @@ import {
   IMG_URL,
   language,
 } from './api.js'
+
+const imagemovie = document.getElementById('image-movie');
+const titlemovie = document.getElementById('tittle-movie');
+const descriptionmovie = document.getElementById('description-movie');
+const btn_random = document.querySelector('button');
+
+btn_random.addEventListener('click', meeetMovies);
+
+meetMovies(); {
+
+  let title
+  let description
+  let image
+
+  axios.get(`${BASE_URL}?api_key=${API_KEY}&${language}`)
+
+  .then(function(response){
+    title = response.data.title;
+    description = response.data.overview;
+    image = `${IMG_URL}/${response.data.poster_path}`;
+
+      if (description.length === 0){
+        description = 'Descrição do filme não encontrada'
+      }
+
+      imagemovie.src = image;
+      titlemovie.innerHTML= title;
+      descriptionmovie.innerHTML = `${description.substring(0, 600)}`;
+      console.log(typeof(response.data.title));
+    })
+
+    .catch(function (error) {
+      console.log(error)
+    })
+
+
+};
+
+
